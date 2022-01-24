165e4acaa957e522b90227771ae0f2a040be0ed2
BrysonFraelich <c22bryson.fraelich@afacademy.af.edu>
2022-01-24 09:59:01 -0700

    cleaned up more of the code to add more consistency between algorithms

--- client.py
+++ client.py
@@ -24,73 +24,110 @@ def with_none(client_socket, message):
 
     t = time.process_time()
 
+    #print("None Message Sent:", message)
+
     client_socket.send(message)  # send message
 
-    data = client_socket.recv(1024)  # receive response
+    message = client_socket.recv(1024)  # receive response
+
+    #print("None message recv:", data)
 
     elapsed_time = (time.process_time()  - t)
     global none_t
     none_t = none_t + elapsed_time
 
-def with_ascon(client_socket, message):
+def with_ascon(client_socket, message, key, nonce, ad, variant):
 
     client_socket.send(b"Ascon")
     recv = client_socket.recv(1024)
 
-    variant = 'Ascon-128'
-    key   = bytes(bytearray([i % 256 for i in range(16)]))
-    nonce = bytes(bytearray([i % 256 for i in range(16)]))
-    ad    = bytes(bytearray([i % 256 for i in range(32)]))
-
     t = time.process_time()
+
+    #print("Ascon Message sent:", message)
 
     message = ascon.ascon_encrypt(key, nonce, ad[:32], message, variant)
 
     client_socket.send(message)  # send message
 
-    data = client_socket.recv(1024)  # receive response
+    message = client_socket.recv(1024)  # receive response
 
-    message = ascon.ascon_decrypt(key, nonce, ad[:32], data, variant) # decrypt received
+    message = ascon.ascon_decrypt(key, nonce, ad[:32], message, variant) # decrypt received
+
+    #print("Ascon message recv:", message)
 
     elapsed_time = (time.process_time()  - t)
 
     global ascon_t
     ascon_t = ascon_t + elapsed_time
 
-def with_aes(client_socket, message):
+def with_aes(client_socket, message, cipher, decipher):
 
     client_socket.send(b"AES")
     recv = client_socket.recv(1024).decode()
 
-    key   = bytes(bytearray([i % 256 for i in range(16)]))
-    cipher = AES.new(key, AES.MODE_ECB)
-    decipher = AES.new(key, AES.MODE_ECB)
-
     t = time.process_time()
+
+    #print("AES message sent:", message)
 
     message = cipher.encrypt(pad(message, BLOCK_SIZE))
 
     client_socket.send(message)
 
-    data = client_socket.recv(1024)  # receive response
+    message = client_socket.recv(1024)  # receive response
 
-    data_dec = decipher.decrypt(data)
+    message = decipher.decrypt(message)
+
+    #print("AES Message recv:", message)
 
     elapsed_time = (time.process_time()  - t)
 
     global aes_t
     aes_t = aes_t + elapsed_time
 
+def trial(message_len, trials, client_socket, key, nonce, ad, variant, cipher, decipher):
+
+    global none_t
+    global ascon_t
+    global aes_t
+    none_t = 0
+    ascon_t = 0
+    aes_t = 0
+
+    print("Message Length:", message_len)
+
+    for i in range(trials): 
+        message = bytes(randomword(message_len), 'utf-8')
+
+        with_none(client_socket, message)
+        with_ascon(client_socket, message, key, nonce, ad, variant)
+        with_aes(client_socket, message, cipher, decipher)
+
+    print("None:", (none_t/2)/trials)
+    print("Ascon:", (ascon_t/2)/trials)
+    print("AES:", (aes_t/2)/trials, "\n")
+
+    return 0
+
 def client_program():
 
     trials = 20
+    message_len = 10
 
-    #host = socket.gethostname()  # as both code is running on same pc
-    host = '10.1.100.218'
+    host = socket.gethostname()  # as both code is running on same pc
+    #host = '10.1.100.218'
     port = 5000 # socket server port number
 
     client_socket = socket.socket()  # instantiate
     client_socket.connect((host, port))  # connect to the server
+
+    variant = 'Ascon-128'
+
+    key   = bytes(bytearray([i % 256 for i in range(16)]))
+    nonce = bytes(bytearray([i % 256 for i in range(16)]))
+    ad    = bytes(bytearray([i % 256 for i in range(32)]))
+
+    cipher = AES.new(key, AES.MODE_ECB)
+    decipher = AES.new(key, AES.MODE_ECB)
 
     global none_t
     global ascon_t
@@ -98,45 +135,11 @@ def client_program():
 
     print("Trials: ", trials)
 
-    for i in range(trials): 
-        message = bytes(randomword(10), 'utf-8')
-
-        with_none(client_socket, message)
-        with_ascon(client_socket, message)
-        with_aes(client_socket, message)
-
-    print("None Time (10):", (none_t/2)/trials)
-    print("Ascon Time (10):", (ascon_t/2)/trials)
-    print("AES Time (10):", (aes_t/2)/trials)
-
-
-    none_t = 0
-    ascon_t = 0
-    aes_t = 0
-    for i in range(trials): 
-        message = bytes(randomword(100), 'utf-8')
-
-        with_none(client_socket, message)
-        with_ascon(client_socket, message)
-        with_aes(client_socket, message)
-
-    print("None Time (100):", (none_t/2)/trials)
-    print("Ascon Time (100):", (ascon_t/2)/trials)
-    print("AES Time (100):", (aes_t/2)/trials)
-
-    none_t = 0
-    ascon_t = 0
-    aes_t = 0
-    for i in range(trials): 
-        message = bytes(randomword(500), 'utf-8')
-
-        with_none(client_socket, message)
-        with_ascon(client_socket, message)
-        with_aes(client_socket, message)
-
-    print("None Time (500):", (none_t/2)/trials)
-    print("Ascon Time (500):", (ascon_t/2)/trials)
-    print("AES Time (500):", (aes_t/2)/trials)
+    trial(message_len, trials, client_socket, key, nonce, ad, variant, cipher, decipher)
+    message_len = 100
+    trial(message_len, trials, client_socket, key, nonce, ad, variant, cipher, decipher)
+    message_len = 500
+    trial(message_len, trials, client_socket, key, nonce, ad, variant, cipher, decipher)
 
     client_socket.send(b'disconnected') # tell the server to disconect
     client_socket.close()  # close the connection
