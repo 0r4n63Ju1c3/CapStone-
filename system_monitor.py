b85b8528622776ffdda03ea500469089d1775943
BrysonFraelich <c22bryson.fraelich@afacademy.af.edu>
2022-02-22 11:06:54 -0700

    csv check

--- system_monitor.py
+++ system_monitor.py
@@ -16,11 +16,16 @@ def get_ram_usage_pct():
 def main():
     print('Testing {} algorithm\n'.format(algo_name))
     start_time = time.time()
+    seconds = 10
     while True:
+        current_time = time.time()
+        elapsed_time = current_time - start_time
         print(dt.now(),",", get_cpu_usage_pct(), ",", int(get_ram_usage() / 1024 / 1024), ",",get_ram_usage_pct())
         #print('Current Time: {} '.format(dt.now()))
         #print('CPU usage is {} %'.format(get_cpu_usage_pct()))
         #print('RAM usage is {} MB'.format(int(get_ram_usage() / 1024 / 1024)))
         #print('RAM usage is {} %'.format(get_ram_usage_pct()))
+        if elapsed_time > seconds:
+            break
 if __name__ == "__main__":
     main()
